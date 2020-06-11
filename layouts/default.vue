efef844ad0646bdb8378afd4c13709ea68ff6eee
ABStein <abs19stein@gmail.com>
2020-06-10 22:36:33 -0500

    feat: add components and set up frontend to consume APIs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="container">
-    <Navbar />
-    <nuxt/>
+  <div>
+    <script defer src="https://kit.fontawesome.com/d11cca7c30.js" crossorigin="anonymous"></script>
+    <div>
+      <Navbar /> 
+    </div>
+    <div class="container">
+      <nuxt/>
+    </div>
   </div>
 </template>
 
@@ -16,8 +21,19 @@ export default {
 
 
 <style>
+:root {
+  /* font-size: 16px;
+  font-family: 'Open Sans'; */
+  --text-primary: #b6b6b6;
+  --text-secondary: #ececec;
+  --bg-primary: #23232e;
+  --bg-secondary: #141418;
+  --transition-speed: 400ms;
+}
+
 * {
   box-sizing: border-box;
+  box-sizing: (max-width 500px);
   margin: 0;
   padding: 0;
 }
@@ -25,20 +41,25 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   font-size: 1rem;
   line-height: 1.6;
-  background: #f4f4f4;
 }
-a {
-  color: #666;
-  text-decoration: none;
-}
+
 ul {
   list-style: none;
 }
+
 .container {
-  max-width: 800px;
-  margin: 2rem auto;
+  margin-left: 80px;
   overflow: hidden;
   padding: 1rem 2rem;
-  background: #fff;
+}
+
+
+/* Small screens */
+@media only screen and (max-width: 600px) {
+  .container {
+    margin-left: 0px;
+    overflow: hidden;
+    padding: 1rem 2rem;
+  }
 }
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to Andrew</h1>
+    <h1>Contact Me</h1>
   </div>
 </template>
 
