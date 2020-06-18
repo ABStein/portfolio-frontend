30b7dcb9f4019b5261438b7dc365566f2c2e202b
ABStein <abs19stein@gmail.com>
2020-06-18 12:11:32 -0500

    feat: add a basic contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="login-content container my-4 mx-auto px-4 md:px-12">
-		<span class="flex justify-center text-4xl">Contact</span>
-  </div>
+  <section>
+    <div class="login-content container my-4 mx-auto px-4 md:px-12">
+      <span class="flex justify-center text-4xl">Contact</span>
+    </div>
+
+    <div class="flex flex-col justify-center items-center">
+      <form class="mt-2" name="contact" method="POST" data-netlify="true">
+        <p class="mt-2">
+          <label>Your Name: <input type="text" name="name" /></label>   
+        </p>
+        <p class="mt-2">
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p class="mt-2">
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p class="mt-2">
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+    
+  </section>
 </template>
 
 <script>
