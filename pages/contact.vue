37f7aa3623b73aa29f79f33c95f136d55d9b6653
ABStein <abs19stein@gmail.com>
2020-06-18 13:35:32 -0500

    functioning form with thank you message

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="container">
-    <div>
-      <h1>{{ teaName }}</h1>
-    </div>
+    <span class="flex justify-center text-4xl">Contact</span>
     <div>
       <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
-        <label for="name">Your Name:</label>
-        <input type="text" name="name" v-model="name">
-        <label for="name">Your Email:</label>
-        <input type="email" name="email" v-model="email">
-        <label for="role">Your Role</label>
-        <select name="role" id="">
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-        <label for="message">Message:</label>
-        <input type="text" name="message" v-model="message">
+        <label class="form-label" for="name">
+          Name:
+        </label>
+        <input class="form-field" name="name" id="name" />
+        <label class="form-label" for="email">
+          Email:
+        </label>
+        <input class="form-field" name="email" id="email" />
+        <label class="form-label" for="message">
+          Message:
+        </label>
+        <textarea class="form-field" name="message" id="message"></textarea>
         <button type="submit" class="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full">
           Submit
         </button>
       </form>
     </div>
   </div>
+
 </template>
 
 <script>
