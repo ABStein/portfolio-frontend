b32c616666a371f0a687564ab0ec3330bcf99592
ABStein <abs19stein@gmail.com>
2020-06-18 15:51:45 -0500

    style the contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,27 +1,40 @@
 <template>
-  <div class="container">
-    <span class="flex justify-center text-4xl">Contact</span>
-    <div>
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="form-name" value="contact" />
-        <label class="form-label" for="name">
-          Name:
-        </label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">
-          Message:
-        </label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <button type="submit" class="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full">
-          Submit
-        </button>
-      </form>
+  <section>
+    <div class="my-4 mx-auto px-4 md:px-12">
+      <span class="flex justify-center text-4xl">Contact</span>
+      <div class="mx-auto w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-4 mb-4" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <div class="mb-4">
+            <input type="hidden" name="form-name" value="contact" />
+            <label class="form-label block text-gray-600 text-sm font-bold mb-2" for="name">
+              Name:
+            </label>
+            <input class="form-field shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline" name="name" id="name" />
+          </div>
+
+          <div class="mb-4">
+            <label class="form-label block text-gray-600 text-sm font-bold mb-2" for="email">
+              Email:
+            </label>
+            <input class="form-field shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline" name="email" id="email" />
+          </div>
+
+          <div class="mb-4">
+            <label class="form-label block text-gray-600 text-sm font-bold mb-2" for="message">
+              Message:
+            </label>
+            <textarea class="form-field shadow appearance-none border rounded w-full py-2 px-3 text-gray-600 leading-tight focus:outline-none focus:shadow-outline" name="message" id="message"></textarea>            
+          </div>
+
+          <div class="flex justify-center mb-4">
+            <button type="submit" class="btn mt-3 bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full">
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </section>
 
 </template>
 
