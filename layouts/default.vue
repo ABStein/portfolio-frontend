43b1c76a88cb04e2b181549a171382180efde6d6
ABStein <abs19stein@gmail.com>
2020-06-18 10:34:20 -0500

    feat: add footer, delete about page due to redundancy, make text color uniform and deleted some readme

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <script defer src="https://kit.fontawesome.com/d11cca7c30.js" crossorigin="anonymous"></script>
       <Navbar /> 
       <nuxt />
+      <Footer />
   </div>
 </template>
 
@@ -12,9 +13,12 @@ import tailwindConfig from '~tailwind.config'
 global.tailwindConfig = tailwindConfig
 
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 };
 </script>
